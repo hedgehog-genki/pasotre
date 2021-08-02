fe9ad06f10e94eb77e1b0e0e5c4a5cdfb73456b7
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-08-02 14:09:46 +0900

    firstcommit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,11 @@ import "packs/application.css";
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,3 +21,15 @@ require("jquery")
 //
 const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//windowが実行された時にどのjsファイルを参照するかの設定
+window.jQuery = window.$ = require("./jquery-1.11.1")
+
+//必要なjsファイルのインポート
+import "./jquery-1.11.1"
+import "./jquery.backstretch"
+import "./placeholder"
+import "./retina-1.1.0"
+import "./scripts"
+import "./waypoints"
+import "./wow"
